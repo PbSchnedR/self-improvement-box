f55020148cb0e92d8ab9d96f1d38d8063fb16fa8
PbSchnedR <pablo.schneider69@gmail.com>
2025-02-20 21:53:10 +0100

    added .env

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Database:
     def __init__(self):
         self.connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="m$0SPl666*hi4nte",
-            database="selfi_box"
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         self.cursor = self.connection.cursor(buffered=True)  # Curseur bufferisé
 
